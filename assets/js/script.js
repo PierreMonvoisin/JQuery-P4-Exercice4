236a504df5e1a5e67c5d367d6b0ee98c5128c3d7
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-11-13 09:57:23 +0100

    Exercice terminé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(function() {
+  // valide les inputs textes
   $('input').not('#birthday').keyup(function nameCheck() {
     let input = ($(this).val()).trim()
     let pattern = /^[a-zA-Z.'-_ ]*$/;
@@ -8,6 +9,7 @@ $(function() {
       $(this).css('border', '3px solid red')
     }
   });
+  //valide l'input date de naissance
   $('#birthday').keyup(function mailCheck() {
     let input = ($(this).val()).trim();
     let pattern = /^([0-9]{2})+\/([0-9]{2})+\/([0-9]{2,4})$/;
@@ -17,10 +19,16 @@ $(function() {
       $(this).css('border', '3px solid red')
     }
   });
+  // Vérifie si un input n'est pas bon, si non, affiche le texte
   $('button').click(function validate(){
-    console.log($(':input').css('border-color'))
-    if ($(':input').css('border-color') == 'rgb(255, 0, 0)'){
-      alert('Veuillez bien remplir tous les champs')
+    let colors = [
+      $('#lName').css('border-color'),$('#fName').css('border-color'),$('#birthday').css('border-color'),$('#birthplace').css('border-color'),$('#job').css('border-color'),$('#workplace').css('border-color')
+    ]
+    console.log(colors)
+    if (colors.includes("rgb(255, 0, 0)")){
+      alert('Merci de bien rentrer vos informations')
+    } else {
+      alert($('#fName').val() + ' ' + ($('#lName').val()).toUpperCase() + ', né le ' + $('#birthday').val() + ' à ' + $('#birthplace').val() + ', actuellement ' + $('#job').val() + ' à ' + $('#workplace').val())
     }
   });
 });
